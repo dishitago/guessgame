3708598f9c0911ef9c9645065c15c090fb525483
dishitago <goswamidishita93@gmail.com>
2026-06-21 17:34:27 +0530

    Rename class from guessgame to GuessGame

--- GuessGame.java
+++ GuessGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class guessgame {
+public class GuessGame {
     public static void main(String[] args) {
         Random ran= new Random();
         int secrtenum=ran.nextInt(50)+1;
